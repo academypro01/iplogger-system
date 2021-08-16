e40099131063913d55dfcd7a7c6a9c7d29ec82bc
academypro01 <80899303+academypro01@users.noreply.github.com>
2021-08-16 15:30:33 +0330

    remove static data

--- dist/app/config/config.php
+++ dist/app/config/config.php
@@ -2,10 +2,10 @@
 date_default_timezone_set("Asia/Tehran");
 // database information
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_USER', '');
 define('DB_PASS', '');
-define('DB_NAME', 'iplogger_dbs');
+define('DB_NAME', '');
 
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'http://localhost/github_repository_projects/iplogger_system/dist/');
+define('URLROOT', '');
 define('SITENAME', 'IP Logger Systetm');
