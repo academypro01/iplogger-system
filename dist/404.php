e51598d01efd203c3b5c3e01fdf7bd5fd8f7c7c5
academypro01 <80899303+academypro01@users.noreply.github.com>
2021-10-05 22:43:51 +0330

    fix core.php errors

---
+++ dist/404.php
@@ -0,0 +1,18 @@
+<?php
+    header("refresh:10, url=index.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+</head>
+<body>
+    <h1>Sorry!</h1>
+    <p>Your file does'nt Exsit !!</p>
+    <hr>
+    <small>You redirect to home page after 10 seconds!</small>
+</body>
+</html>
--- dist/app/config/config.php
+++ dist/app/config/config.php
@@ -1,4 +1,5 @@
 <?php
+// EDIT THIS FILE ONLY! SAVE -> RUN -> ENJOY
 date_default_timezone_set("Asia/Tehran");
 // database information
 define('DB_HOST', 'localhost');
@@ -6,6 +7,11 @@ define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
 
+// root local path
 define('APPROOT', dirname(dirname(__FILE__)));
+
+// root url address
 define('URLROOT', '');
+
+// your site name
 define('SITENAME', 'IP Logger Systetm');
